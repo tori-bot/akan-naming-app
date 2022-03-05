e73b47976792214f7d8737c5868200c9680608fb
victoria makena <makenavictoria1@gmail.com>
2022-03-05 11:12:36 +0300

    define prompts

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 var username = prompt('what is your name?');
 var gender = prompt('what is your gender? :');
-var cc = prompt('your century digits (eg 19 for 1989) are :');
-var yy = prompt('your year digits (eg 89 for 1989) are :');
-var mm = prompt('your birth month is :');
-var dd = prompt('your day of birth is :');
+var cc = parseInt(prompt('your century digits (eg 19 for 1989) are :'));
+var yy = parseInt(prompt('your year digits (eg 89 for 1989) are :'));
+var mm = parseInt(prompt('number of your birth month is :'));
+var dd = parseInt(prompt('your day of birth is :'));
 
 var malenames = { 1: 'kwasi', 2: 'kwadwo', 3:'',4:'',5:'',6:'',7:''}
