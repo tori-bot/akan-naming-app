3b824b1b7b891ed399801d85c65f79c3b0c3c3f1
victoria makena <makenavictoria1@gmail.com>
2022-03-05 11:08:38 +0300

    add gender and date of birth prompts

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 var username = prompt('what is your name?');
-var dateofbirth = prompt('your date of birth is :');
+var gender = prompt('what is your gender? :');
+var cc = prompt('your century digits (eg 19 for 1989) are :');
+var yy = prompt('your year digits (eg 89 for 1989) are :');
+var mm = prompt('your birth month is :');
+var dd = prompt('your day of birth is :');
 
-var malenames={1:'kwasi',2:'kwadwo',3:}
+var malenames = { 1: 'kwasi', 2: 'kwadwo', 3:'',4:'',5:'',6:'',7:''}
