d4ca3cd97ccc5150d91cb9950ffe647ebdefe6a6
victoria makena <makenavictoria1@gmail.com>
2022-03-05 11:29:47 +0300

    define key value pair arrays of names

--- js/script.js
+++ js/script.js
@@ -5,4 +5,5 @@ var yy = parseInt(prompt('your year digits (eg 89 for 1989) are :'));
 var mm = parseInt(prompt('number of your birth month is :'));
 var dd = parseInt(prompt('your day of birth is :'));
 
-var malenames = { 1: 'kwasi', 2: 'kwadwo', 3:'',4:'',5:'',6:'',7:''}
+var malenames = { 1: 'Kwasi', 2: 'Kwadwo', 3: 'Kwabena', 4: 'Kwaku', 5: 'Yow', 6: 'Kofi', 7: 'Kwame' };
+var femalenames = { 1: 'Akosua', 2: 'Adwoa', 3: 'Abenaa', 4: 'Akua', 5: 'Yaa', 6: 'Afua', 7: 'Ama' };
