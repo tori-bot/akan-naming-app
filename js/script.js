a9b29c80af8c4949ae7c09fb67e5e0b842557b6e
victoria makena <makenavictoria1@gmail.com>
2022-03-06 02:35:18 +0300

    modify business logic section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-var username = prompt('what is your name?');
-var gender = prompt('what is your gender? :');
-var cc = parseInt(prompt('your century digits (eg 19 for 1989) are :'));
-var yy = parseInt(prompt('your year digits (eg 89 for 1989) are :'));
-var mm = parseInt(prompt('number of your birth month is :'));
-var dd = parseInt(prompt('your day of birth is :'));
+// business logic
+var malenames = ['kwasi', 'kwadwo', 'kwabena', 'kwaku', 'yow', 'kofi', 'kwame'];
+var femalenames = ['akosua', 'adwoa', 'abenaa', 'akua', 'yaa', 'afua', 'ama'];
+var daysofweek=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 
-var malenames = { 1: 'Kwasi', 2: 'Kwadwo', 3: 'Kwabena', 4: 'Kwaku', 5: 'Yow', 6: 'Kofi', 7: 'Kwame' };
-var femalenames = { 1: 'Akosua', 2: 'Adwoa', 3: 'Abenaa', 4: 'Akua', 5: 'Yaa', 6: 'Afua', 7: 'Ama' };
+var username = prompt('what is your name?');
+var gender = parseInt(prompt('what is your gender? (0=male,1=female) :'));
+var CC = parseInt(prompt('your century digits (eg 19 for 1989) are :'));
+var YY = parseInt(prompt('your year digits (eg 89 for 1989) are :'));
+var MM = parseInt(prompt('number of your birth month is :'));
+var DD = parseInt(prompt('your day of birth is :'));
+
+
+
+
+
