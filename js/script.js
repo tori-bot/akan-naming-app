6282ad5b23c9aa84156725439e67fbbfdd5559c0
victoria makena <makenavictoria1@gmail.com>
2022-03-06 03:15:06 +0300

    modify user interface section

--- js/script.js
+++ js/script.js
@@ -3,12 +3,17 @@ var malenames = ['kwasi', 'kwadwo', 'kwabena', 'kwaku', 'yow', 'kofi', 'kwame'];
 var femalenames = ['akosua', 'adwoa', 'abenaa', 'akua', 'yaa', 'afua', 'ama'];
 var daysofweek=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 
-var username = prompt('what is your name?');
-var gender = parseInt(prompt('what is your gender? (0=male,1=female) :'));
-var CC = parseInt(prompt('your century digits (eg 19 for 1989) are :'));
-var YY = parseInt(prompt('your year digits (eg 89 for 1989) are :'));
-var MM = parseInt(prompt('number of your birth month is :'));
-var DD = parseInt(prompt('your day of birth is :'));
+// user interface
+var year = parseInt(document.getElementById('year').value);
+var month = parseInt(document.getElementById('month').value);
+var day = parseInt(document.getElementById('day').value);
+var gender = radioInfo();
+
+var dayofweek = new Date(year + '/' + month + '/' + day);
+var siku = dayofweek.getDay();
+var name = "";
+
+if gender==='male'
 
 
 
