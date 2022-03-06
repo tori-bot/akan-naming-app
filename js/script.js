673bd66125e03c0abc0d32e6b348b22a9c627ff3
victoria makena <makenavictoria1@gmail.com>
2022-03-06 16:18:05 +0300

    modify user interface

--- js/script.js
+++ js/script.js
@@ -4,29 +4,38 @@ var femalenames = ['akosua', 'adwoa', 'abenaa', 'akua', 'yaa', 'afua', 'ama'];
 var daysofweek=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 
 // user interface
-function form(){
-var year = parseInt(document.getElementById('year').value);
-var month = parseInt(document.getElementById('month').value);
-var day = parseInt(document.getElementById('day').value);
-var gender = radioInfo();
+function getInfo() {
+    var year = parseInt(document.getElementById('year').value);
+    var month = parseInt(document.getElementById('month').value);
+    var day = parseInt(document.getElementById('day').value);
+    var gender = radioInfo();
 
-var dayofweek = new Date(year + '/' + month + '/' + day);
-var siku = dayofweek.getDay();
-var Aname = "";
+    var dayofweek = new Date(year + '/' + month + '/' + day);
+    var siku = daysofweek.getDay();
+    var Aname = "";
 
-if (gender === 'male')
-{
-    Aname = male[siku];
-}
-else
-{
-    Aname = female[siku];
-}
+    if (gender === 'male')
+    {
+        Aname = male[siku];
+    }
+    else
+    {
+        Aname = female[siku];
+    }
     alert('your birthday was on a ' + daysofweek(siku) + 'and your Akan name is ' + Aname);
-    // document.getElementById('akan').innerHTML=your birthday was on a ' + daysofweek(siku) + 'and your Akan name is ' + Aname;
-    
-
+     document.getElementById('akan').innerHTML= 'your birthday was on a ' + daysofweek(siku) + 'and your Akan name is ' + Aname;
+    }
+function radioInfo() {
+    var radio = document.getElementsByName('gender');
+    for (var i = 0; i < radio.length; i++) {
+        if (radio[i].checked == true) {
+            gender = radio[i].value;
+            return gender;
+        }
+    }
 }
+
+
 
 
 
