956a6d060608e1493c4bf58344dcff64b1714c44
victoria makena <makenavictoria1@gmail.com>
2022-03-07 04:00:41 +0300

    correct list of days of week

--- js/script.js
+++ js/script.js
@@ -1,45 +1,54 @@
-var male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+let male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-var dateWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let dateWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-//userInterface
+//user Interface
 
-function getInfo(){
-    var day = parseInt(document.getElementById("day").value);
-    
-    var month = parseInt(document.getElementById("month").value);
-    
-    var year = parseInt(document.getElementById("year").value);
-    
-    var gender = radioInfo();
-    
-    var dayWeek = new Date(year + "/" + month + "/" + day);
-    var d = dayWeek.getDay();
-    var name="" ;
-    if (gender ==="male"){
-       name = male[d];
+function getInfo() {
+  let day = parseInt(document.getElementById("day").value);
+
+  let month = parseInt(document.getElementById("month").value);
+
+  let year = parseInt(document.getElementById("year").value);
+
+  let gender = radioInfo();
+
+  let dayWeek = new Date(year + "/" + month + "/" + day);
+  let d = dayWeek.getDay();
+  let name = "";
+  if (gender === "male") {
+    name = male[d];
+  } else {
+    name = female[d];
+  }
+  //concatenate
+  alert("You were born on " + dateWeek[d] + " and your Akan name is " + name);
+  document.getElementById("akan").innerHTML =
+    "You were born on " +
+    daysOfTheWeek[d] +
+    " and your Akan name is: " +
+    maleNames[d];
+}
+function radioInfo() {
+  let radio = document.getElementsByName("gender");
+  for (let i = 0; i < radio.length; i++) {
+    if (radio[i].checked == true) {
+      // generate output with the checked radio
+
+      gender = radio[i].value;
+      // only one radio can be logically checked
+
+      return gender;
     }
-    else{
-       name = female[d];
-    }
-    //concat the day born and the Akan name
-    alert("You were born on "+ dateWeek[d] + " and your Akan name is "+ name);
-document.getElementById('akan').innerHTML = "You were born on " + daysOfTheWeek[d] +  " and your Akan name is: " + maleNames[d] ;
-}
-function radioInfo (){
-    var radio = document.getElementsByName('gender');
-    for (var i = 0;  i<radio.length; i++)
-    {
-    if (radio[i].checked==true)
-    {
-        // generate output with the checked radio
-
-        gender = radio[i].value;
-        // only one radio can be logically checked
-
-return gender;
-}
-}
+  }
 }
